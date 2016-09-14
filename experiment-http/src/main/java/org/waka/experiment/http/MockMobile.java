6a93cd4a8d2c900d457cb0fb90a4cb90b99861f7
Coner <wangkang1102@hotmail.com>
2016-09-14 09:32:44 +0800

    增加NIO

---
+++ experiment-http/src/main/java/org/waka/experiment/http/MockMobile.java
@@ -0,0 +1,55 @@
+package org.waka.experiment.http;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by coner on 16-9-13.
+ */
+public class MockMobile {
+
+    public static void doTest() throws IOException {
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost("http://targethost/login");
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        nvps.add(new BasicNameValuePair("username", "vip"));
+        nvps.add(new BasicNameValuePair("password", "secret"));
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+        try {
+            System.out.println(response2.getStatusLine());
+            HttpEntity entity2 = response2.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity2);
+        } finally {
+            response2.close();
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            doTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/waka/experiment/classloader/NetClassLoader.java
@@ -0,0 +1,9 @@
+package org.waka.experiment.classloader;
+
+/**
+ * Created by coner on 16-9-13.
+ */
+public class NetClassLoader extends ClassLoader{
+
+
+}
